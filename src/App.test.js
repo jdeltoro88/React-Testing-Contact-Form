6511627439f802bca2e9c98f5529c4d7057a9653
Jonathan Deltoro <jonathandeltoro@Jonathans-MacBook-Air.local>
2020-09-16 19:44:08 -0600

    tests paasing

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,24 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
+
+//Arrange
+//Act
+//Assert
+
+test("App renders Name field", () => {
+  const { getByText } = render(<App />);
+  const nameField = getByText(/First Name/i)
+  expect(nameField).toBeInTheDocument
+});
+
+test("App renders Last name field", () => {
+  const { getByText } = render(<App />);
+  const nameLastField = getByText(/Last Name/i)
+  expect(nameLastField).toBeInTheDocument
+});
+test("App renders email field", () => {
+  const { getByText } = render(<App />);
+  const emailField = getByText(/Email/i)
+  expect(emailField).toBeInTheDocument
 });
